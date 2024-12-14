c2e16b77d4756da3fedbad659d09da2c2dd490f0
mohit kala jain <mohitkala0@gmail.com>
2024-12-14 11:51:26 +0530

    How to Use Array inside Component

--- ReStore/Client/src/App.tsx
+++ ReStore/Client/src/App.tsx
@@ -1,10 +1,19 @@
-import './App.css'
 
+const products=[
+{name:'Product 1',price:100.00},
+{name:'Product 2',price:200.00},
+]
 function App() {
 
   return (
     <div>
       <h1>Re-Store</h1>
+      <ul>
+        
+        {products.map((item,index)=>(
+          <li key={index}>{item.name} - {item.price}</li>
+        ))}
+      </ul>
     </div>
   )
 }
