7493c39e83eae50c449747bdc18119990abb4cf7
mohit kala jain <mohitkala0@gmail.com>
2024-12-14 12:20:13 +0530

    useEffect and Cors

--- ReStore/API/obj/Debug/net7.0/API.AssemblyInfo.cs
+++ ReStore/API/obj/Debug/net7.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ece0b826742084eb65907f3f3256d22065e86874")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d4c700ddb9c8e3b1383265b87924700d7b499cb8")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- ReStore/Client/src/App.tsx
+++ ReStore/Client/src/App.tsx
@@ -1,29 +1,40 @@
-import { useState } from "react"
+import { useEffect, useState } from "react";
 
 function App() {
-const [products,setProducts]=useState(
-  [
-    {name:'Product 1',price:100.00},
-    {name:'Product 2',price:200.00},
-    ]
-)
+  const [products, setProducts] = useState([
+    { name: "Product 1", price: 100.0 },
+    { name: "Product 2", price: 200.0 },
+  ]);
 
-function addProduct(){
-  setProducts(prevState=> [...prevState,{name:'Product ' +(prevState.length+1),price:(prevState.length * 100)+ 100}])
-}
+  useEffect(() => {
+    fetch("http://localhost:5000/api/products")
+      .then((response) => response.json())
+      .then((data) => setProducts(data));
+  }, []);
+
+  function addProduct() {
+    setProducts((prevState) => [
+      ...prevState,
+      {
+        name: "Product " + (prevState.length + 1),
+        price: prevState.length * 100 + 100,
+      },
+    ]);
+  }
 
   return (
     <div>
       <h1>Re-Store</h1>
       <ul>
-        
-        {products.map((item,index)=>(
-          <li key={index}>{item.name} - {item.price}</li>
+        {products.map((item, index) => (
+          <li key={index}>
+            {item.name} - {item.price}
+          </li>
         ))}
       </ul>
       <button onClick={addProduct}>Add Product</button>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
